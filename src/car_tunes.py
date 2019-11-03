076a19f005175a4fbaf3d0ebcfe6e3f71fa3e1e5
Ryan Todd <ryan.todd@BusinessMicros.onmicrosoft.com>
2019-11-03 23:38:59 +0000

    Implement vlc for audio playback

--- src/car_tunes.py
+++ src/car_tunes.py
@@ -3,37 +3,35 @@ import re
 import sys,os
 import time
 import threading
+import vlc
 from curses import wrapper
 from os import listdir
 from os.path import isfile, join
 from time import sleep
 
 working = True
-x = 0
-last_key = 0
-updated = True
-music_dir = "/home/user/python/environments/thing1/src/music"
+screen_update = True
+status_update = True
+is_playing = True
+music_dir = "C:\\Users\\RyanT\\Documents\\Personal\\music\\Converted Library"
 loaded_artists = []
 loaded_albums = []
 loaded_tracks = []
 artist_index = 0
 album_index = 0
 track_index = 0
-left_status = "Paused"
-right_status = "00:00 / 03:45"
+active_player = None
 
 def draw_menu(stdscr):
     global working
-    global x
-    global updated
-    global last_key
+    global screen_update
+    global status_update
 
     display_weight_artist = 1
     display_weight_album = 1
     display_weight_track = 2
 
     curses.curs_set(0)
-
     stdscr.clear()
     stdscr.refresh()
 
@@ -42,58 +40,88 @@ def draw_menu(stdscr):
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, 3, 5)
 
+    height, width = stdscr.getmaxyx()
+
+    tick = 0
     while working:
-        sleep(0.05)
-        if not updated:
-            continue
+        tick = tick + 1
+        sleep(0.02)
 
-        updated = False
-        stdscr.clear()
-        height, width = stdscr.getmaxyx()
+        if tick % 2 == 0:
+            status_update = True
 
-        display_weight_total = display_weight_artist + display_weight_album + display_weight_track
-        artist_x = 0
-        album_x = int(round((width - 2) * (display_weight_album / display_weight_total)))
-        track_x = int(round((width - 2) * ((display_weight_album + display_weight_album) / display_weight_total)))
-        artist_w = album_x - 1
-        album_w = track_x - (album_x + 1)
-        track_w = width - track_x
+        if active_player is not None:
+            if active_player.get_state() == 6:
+                next_track(False)
+                return
 
-        mid_y = int(height//2) - (1 if (height % 2) == 0 else 0)
-        lines_above = mid_y - 3
-        lines_below = height - (mid_y + 4)
-        artist_list_index, artist_list = screenslice_items(loaded_artists, artist_index, lines_above, lines_below)
-        album_list_index, album_list = screenslice_items(loaded_albums, album_index, lines_above, lines_below)
-        track_list_index, track_list = screenslice_items(loaded_tracks, track_index, lines_above, lines_below)
+        if screen_update or status_update:
+            height, width = stdscr.getmaxyx()
 
-        stdscr.attron(curses.color_pair(1))
-        stdscr.attron(curses.A_DIM)
+        if screen_update:
+            screen_update = False
+            status_update = True
+            stdscr.clear()
 
-        hor_line = "─" * width
-        for y in 1, mid_y - 1, mid_y + 1, height - 2:
-            stdscr.addstr(y, 0, hor_line)
+            display_weight_total = display_weight_artist + display_weight_album + display_weight_track
+            artist_x = 0
+            album_x = int(round((width - 2) * (display_weight_album / display_weight_total)))
+            track_x = int(round((width - 2) * ((display_weight_album + display_weight_album) / display_weight_total)))
+            artist_w = album_x - 1
+            album_w = track_x - (album_x + 1)
+            track_w = width - track_x
 
-        for y in range(0, height - 1):
-            char = "┼" if y in [1, mid_y - 1, mid_y + 1] else "┴" if y == (height - 2) else "│"
-            for x in album_x - 1, track_x - 1:
-                stdscr.addstr(y, x, char)
+            mid_y = int(height//2) - (1 if (height % 2) == 0 else 0)
+            lines_above = mid_y - 3
+            lines_below = height - (mid_y + 4)
+            artist_list_index, artist_list = screenslice_items(loaded_artists, artist_index, lines_above, lines_below)
+            album_list_index, album_list = screenslice_items(loaded_albums, album_index, lines_above, lines_below)
+            track_list_index, track_list = screenslice_items(loaded_tracks, track_index, lines_above, lines_below)
 
-        stdscr.addstr(0, artist_x + int((artist_w - 6) / 2), "Artist")
-        stdscr.addstr(0, album_x + int((album_w - 5) / 2), "Album")
-        stdscr.addstr(0, track_x + int((track_w - 5) / 2), "Track")
+            stdscr.attron(curses.color_pair(1))
+            stdscr.attron(curses.A_DIM)
 
-        stdscr.addstr(height - 1, 1, left_status)
-        stdscr.addstr(height - 1, width - (len(right_status) + 1), right_status)
+            hor_line = "─" * width
+            for y in 1, mid_y - 1, mid_y + 1, height - 2:
+                stdscr.addstr(y, 0, hor_line)
 
-        debug_message = "Testing!"
-        stdscr.addstr(height - 1, (width - len(debug_message)) // 2, debug_message)
+            for y in range(0, height - 1):
+                char = "┼" if y in [1, mid_y - 1, mid_y + 1] else "┴" if y == (height - 2) else "│"
+                for x in album_x - 1, track_x - 1:
+                    stdscr.addstr(y, x, char)
 
-        stdscr.attroff(curses.color_pair(1))
-        stdscr.attroff(curses.A_DIM)
+            stdscr.addstr(0, artist_x + int((artist_w - 6) / 2), "Artist")
+            stdscr.addstr(0, album_x + int((album_w - 5) / 2), "Album")
+            stdscr.addstr(0, track_x + int((track_w - 5) / 2), "Track")
 
-        display_column(stdscr, artist_x, artist_w, mid_y, artist_list_index, artist_list)
-        display_column(stdscr, album_x, album_w, mid_y, album_list_index, album_list)
-        display_column(stdscr, track_x, track_w, mid_y, track_list_index, track_list)
+            stdscr.attroff(curses.color_pair(1))
+            stdscr.attroff(curses.A_DIM)
+
+            display_column(stdscr, artist_x, artist_w, mid_y, artist_list_index, artist_list)
+            display_column(stdscr, album_x, album_w, mid_y, album_list_index, album_list)
+            display_column(stdscr, track_x, track_w, mid_y, track_list_index, track_list)
+
+        if status_update:
+            status_update = False
+            stdscr.attron(curses.color_pair(1))
+            stdscr.attron(curses.A_BOLD)
+
+            left_status = "Playing" if is_playing else "Paused "
+
+            if active_player is None:
+                right_status = "00:00 / 00:00"
+            else:
+                t_mm, t_ss   = divmod(active_player.get_length() / 1000, 60)
+                c_mm, c_ss   = divmod(active_player.get_time() / 1000, 60)
+                remaining = active_player.get_state()
+                right_status = ("%02d:%02d" % (c_mm, c_ss)) + (" / %02d:%02d" % (t_mm, t_ss))
+
+
+            stdscr.addstr(height - 1, 1, left_status)
+            stdscr.addstr(height - 1, width - (len(right_status) + 1), right_status)
+
+            stdscr.attroff(curses.color_pair(1))
+            stdscr.attroff(curses.A_BOLD)
 
         stdscr.refresh()
 
@@ -136,11 +164,32 @@ def screenslice_items(items, index, lines_above, lines_below):
 
     return sliced_index, sliced_items
 
+def next_track(direction, reverse_album, reverse_track):
+    global track_index
+
+    track_index = (track_index + (len(loaded_tracks) + direction)) % len(loaded_tracks)
+    if track_index == (0 if direction == 1 else len(loaded_tracks) - 1):
+        next_album(direction, reverse_album, reverse_track)
+    else:
+        load_track()
+
+def next_album(direction, reverse_album, reverse_track):
+    global album_index
+
+    album_index = (album_index + (len(loaded_albums) + direction)) % len(loaded_albums)
+    if album_index == (0 if direction == 1 else len(loaded_albums) - 1):
+        next_artist(direction, reverse_album, reverse_track)
+    else:
+        load_tracks(reverse_track)
+
+def next_artist(direction, reverse_album, reverse_track):
+    global artist_index
+
+    artist_index = (artist_index + (len(loaded_artists) + direction)) % len(loaded_artists)
+    load_albums(reverse_album, reverse_track)
+
 def input_worker(stdscr):
     global working
-    global x
-    global updated
-    global last_key
     global artist_index
     global album_index
     global track_index
@@ -148,28 +197,23 @@ def input_worker(stdscr):
     while working:
         c = stdscr.getch()
         curses.flushinp()
-        last_key = c
 
         if c == ord('q'):
-            artist_index = (artist_index + (len(loaded_artists) - 1)) % len(loaded_artists)
-            load_albums()
+            next_artist(-1, False, False)
         elif c == ord('a'):
-            artist_index = (artist_index + 1) % len(loaded_artists)
-            load_albums()
+            next_artist(1, False, False)
         elif c == ord('w'):
-            album_index = (album_index + (len(loaded_albums) - 1)) % len(loaded_albums)
-            load_tracks()
+            next_album(-1, True, False)
         elif c == ord('s'):
-            album_index = (album_index + 1) % len(loaded_albums)
-            load_tracks()
+            next_album(1, False, False)
         elif c == ord('e'):
-            track_index = (track_index + (len(loaded_tracks) - 1)) % len(loaded_tracks)
+            next_track(-1, True, True)
         elif c == ord('d'):
-            track_index = (track_index + 1) % len(loaded_tracks)
+            next_track(1, False, False)
         elif c == ord('z'):
             working = False
-
-        updated = True
+        elif c == ord('p'):
+            pause_track_toggle()
 
 def load_artists():
     global loaded_artists
@@ -177,23 +221,47 @@ def load_artists():
     artist_index = 0
     artist_dir = music_dir
     loaded_artists = sorted_nicely([f for f in listdir(artist_dir) if not isfile(join(artist_dir, f))])
-    load_albums()
+    load_albums(False, False)
 
-def load_albums():
+def load_albums(reverse_album, reverse_track):
     global loaded_albums
     global album_index
-    album_index = 0
     album_dir = join(music_dir, loaded_artists[artist_index])
     loaded_albums = sorted_nicely([f for f in listdir(album_dir) if not isfile(join(album_dir, f))])
-    load_tracks()
+    album_index = 0 if not reverse_album else len(loaded_albums) - 1
+    load_tracks(reverse_track)
 
-def load_tracks():
+def load_tracks(reverse_track):
     global loaded_tracks
     global track_index
     global updated
-    track_index = 0
     track_dir = join(music_dir, loaded_artists[artist_index], loaded_albums[album_index])
     loaded_tracks = sorted_nicely([f for f in listdir(track_dir) if isfile(join(track_dir, f))])
+    track_index = 0 if not reverse_track else len(loaded_tracks) - 1
+    load_track()
+
+def load_track():
+    global active_player
+    global is_playing
+    global screen_update
+    if active_player is not None:
+        active_player.stop()
+
+    path = join(music_dir, loaded_artists[artist_index], loaded_albums[album_index], loaded_tracks[track_index])
+    active_player = vlc.MediaPlayer(path)
+    active_player.play()
+    is_playing = True
+    screen_update = True
+
+def pause_track_toggle():
+    global is_playing
+    global status_update
+    if active_player is None:
+        return
+
+    active_player.pause()
+    is_playing = not is_playing
+    status_update = True
 
 def sorted_nicely(l):
     convert = lambda text: int(text) if text.isdigit() else text
